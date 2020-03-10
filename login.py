1adff50cf36aceb625ee5cce7cb0a05f0841693c
Jordan Reger <33134197+jordanreger@users.noreply.github.com>
2020-03-10 10:00:00 -0400

    Create login.py

---
+++ login.py
@@ -0,0 +1,61 @@
+import json, hashlib, os, time, sys, getpass
+
+def login():
+  username = input("Username: ")
+  file = open(f'{username}.json')
+  user_dict = json.load(file)
+  username = username.encode('utf-8')
+  userhash = hashlib.sha256(username).hexdigest()
+  if(userhash == user_dict['username']):
+    password = getpass.getpass("Password: ")
+    password = password.encode('utf-8')
+    passhash = hashlib.sha256(password).hexdigest()
+    if(passhash == user_dict['password']):
+      print("Logged in...\n")
+      time.sleep(0.25)
+      clear()
+      #call function that you want to run after login
+    else:
+      print("Invalid password.")
+      time.sleep(1)
+      clear()
+      menu()
+    
+
+def new_user():
+  username = input("Username: ")
+
+  if(os.path.exists(f'{username}.json') != True):
+    password = getpass.getpass("Password: ")
+    if(username != '' and password != ''):
+      with open(f'{username}.json', 'w+') as file:
+        username = username.encode('utf-8')
+        password = password.encode('utf-8')
+        userhash = hashlib.sha256(username).hexdigest()
+        passhash = hashlib.sha256(password).hexdigest()
+
+        user_json = {
+        'username': userhash,
+        'password': passhash
+        }
+
+        file.write(json.dumps(user_json, indent=4))
+        file.close()
+  else:
+    print("Username taken.")
+    time.sleep(1)
+    clear()
+    menu()
+
+def menu():
+  userInput = input("Login or sign up: ")
+  if(userInput.lower() == 'login'):
+    login()
+  elif(userInput.lower() == 'sign up'):
+    new_user()
+
+def clear():
+    os.system("clear")
+    
+#start program
+menu()
